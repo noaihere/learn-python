459796e84495056a47ce3f59e107ad4d92e5ef70
noaihere <https://github.com/osakahitodesu.com>
2019-04-09 21:16:26 +0800

    test edit

--- src/getting_started/test_variables.py
+++ src/getting_started/test_variables.py
@@ -27,10 +27,10 @@ def test_variables():
     """Test variables"""
 
     integer_variable = 5
-    string_variable = 'John'
+    string_variable = 'Johnn'
 
     assert integer_variable == 5
-    assert string_variable == 'John'
+    assert string_variable == 'Johnn'
 
     variable_with_changed_type = 4  # x is of type int
     variable_with_changed_type = 'Sally'  # x is now of type str
